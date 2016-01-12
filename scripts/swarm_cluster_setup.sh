bb6cfb1757988316f048a0f74af1e388ffa2dca5
eftov <b.fovet@gmail.com>
2016-01-12 13:19:09 +0100

    Create more slave nodes and teardown cluster

--- scripts/swarm_cluster_setup.sh
+++ scripts/swarm_cluster_setup.sh
@@ -9,14 +9,17 @@ docker-machine create \
   --engine-opt="cluster-advertise=eth1:2376" \
   swarm-master
 
-# And a swarm slave
-docker-machine create \
-  -d virtualbox \
-  --swarm --swarm-image="swarm:latest" \
-  --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" \
-  --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" \
-  --engine-opt="cluster-advertise=eth1:2376" \
-  swarm-node-01
+# Create 3 swarm slaves
+for i in {1..3}
+do
+  docker-machine create \
+    -d virtualbox \
+    --swarm --swarm-image="swarm:latest" \
+    --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" \
+    --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" \
+    --engine-opt="cluster-advertise=eth1:2376" \
+    swarm-node-$i
+done
 
 # Digital Ocean setup
 #  docker-machine create \
