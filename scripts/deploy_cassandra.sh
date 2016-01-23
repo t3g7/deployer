3f91facb2fac3618cd9bc2c58f8d3c02740cc765
eftov <b.fovet@gmail.com>
2016-01-23 19:47:21 +0100

    Add listen address and port for connecting to Cassandra

--- scripts/deploy_cassandra.sh
+++ scripts/deploy_cassandra.sh
@@ -2,27 +2,39 @@
 
 echo "Deploying container cass1 on swarm-node-1"
 docker run --name cass1 -d \
+  -h cass1 \
   -e CASSANDRA_BROADCAST_ADDRESS=$(docker-machine ip swarm-node-1) \
+  -e CASSANDRA_RPC_ADDRESS=0.0.0.0 \
+  -e CASSANDRA_START_RPC=true \
   -e "constraint:node==swarm-node-1" \
   -p 7000:7000 \
+  -p 9042:9042 \
   cassandra:2.2.4
 
 echo "Deploying container cass2 on swarm-node-2"
 docker run --name cass2 -d \
+  -h cass2 \
   -e CASSANDRA_BROADCAST_ADDRESS=$(docker-machine ip swarm-node-2) \
+  -e CASSANDRA_RPC_ADDRESS=0.0.0.0 \
+  -e CASSANDRA_START_RPC=true \
   -e CASSANDRA_SEEDS=$(docker-machine ip swarm-node-1) \
   -e "constraint:node==swarm-node-2" \
   -p 7000:7000 \
+  -p 9042:9042 \
   cassandra:2.2.4
 
 echo "Deploying container cass3 on swarm-node-3"
 docker run --name cass3 -d \
+  -h cass3 \
   -e CASSANDRA_BROADCAST_ADDRESS=$(docker-machine ip swarm-node-3) \
+  -e CASSANDRA_RPC_ADDRESS=0.0.0.0 \
+  -e CASSANDRA_START_RPC=true \
   -e CASSANDRA_SEEDS=$(docker-machine ip swarm-node-1) \
   -e "constraint:node==swarm-node-3" \
   -p 7000:7000 \
+  -p 9042:9042 \
   cassandra:2.2.4
 
 echo "Waiting for the cluster to be completely up..."
-sleep 15
+sleep 30
 docker run -it --rm --net container:cass1 cassandra:2.2.4 nodetool status
